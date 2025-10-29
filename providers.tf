ecf85bf99ba4237f971ffaebe582b48503817f40
hamza hssaini <hamzahssaini0@gmail.com>
2025-10-29 20:05:54 +0000

    Updated providers.tf

--- providers.tf
+++ providers.tf
@@ -8,6 +8,8 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  subscription_id   =  "6fc13d10-c1bf-4274-8d91-68859a20ab08"
-  tenant_id         = "129d979e-2acc-4867-8f4f-950622f47afb"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
